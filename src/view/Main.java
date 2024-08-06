8ded69324ef688f1f3b126ba41c628956aac7d67
RafaelLSartori <rafa_sartori55@hotmail.com>
2024-08-06 17:23:33 -0300

    Exercício Recursividade 3

--- src/view/Main.java
+++ src/view/Main.java
@@ -5,20 +5,6 @@ import controller.*;
 public class Main {
     public static void main(String[] args) {
 
-        //Exercício 1
-        Recursividade01 m1 = new Recursividade01();
-        int n1 = 7;
-        int resposta1 = m1.somaNaturais(n1);
-        System.out.println(resposta1);
-
-        //Exercício 2
-        Recursividade02 m2 = new Recursividade02();
-        int[] vet2 = {12, 3, 2, 10, 5};
-        int menor = vet2[4];
-        int tamanho = vet2.length;
-        int resposta2 = m2.menorValorVetor(vet2, menor, tamanho);
-        System.out.println(resposta2);
-
         //Exercício 3
         Recursividade03 m3 = new Recursividade03();
         int fat = 1;
@@ -26,17 +12,5 @@ public class Main {
         int resposta3 = m3.fatorial(n3, fat);
         System.out.println(resposta3);
 
-        //Exercício 4
-        Recursividade04 m4 = new Recursividade04();
-        int[] vet4 = {-12, -2, 3, -10, 5};
-        tamanho = vet4.length;
-        int resposta4 = m4.vetorNegativo(vet4, tamanho);
-        System.out.println(resposta4);
-
-        //Exercício 5
-        Recursividade05 m5 = new Recursividade05();
-        double n5 = 4;
-        double resposta5 = m5.serie(n5);
-        System.out.println(resposta5);
     }
 }
