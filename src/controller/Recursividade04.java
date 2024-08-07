c5e2914d78754273cb02e0f854dc49d98dfd7693
RafaelLSartori <rafa_sartori55@hotmail.com>
2024-08-07 15:56:35 -0300

    Atualização

--- src/controller/Recursividade04.java
+++ src/controller/Recursividade04.java
@@ -6,13 +6,15 @@ public class Recursividade04 {
     }
     int total = 0;
     public int vetorNegativo(int[] vet, int tamanho){
+        //O ponto de parada foi pensado na mesma forma do exercício anterior com vetor, percorrendo todo ele.
         if(tamanho == 0){
             return total;
         }
+        //Aqui eu faço a contagem dos números negativos.
         if(vet[tamanho - 1] < 0){
             total++;
         }
-
+        //Chamada da função recursiva reduzindo o índice do vetor para percorrer ele todo.
         return vetorNegativo(vet, tamanho - 1);
     }
 }
