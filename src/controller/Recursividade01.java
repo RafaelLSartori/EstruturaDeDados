ddd62e8a60445b055bb9e8ebb79dbd347cc68b57
RafaelLSartori <rafa_sartori55@hotmail.com>
2024-08-07 15:38:49 -0300

    Atualização

--- src/controller/Recursividade01.java
+++ src/controller/Recursividade01.java
@@ -7,9 +7,11 @@ public class Recursividade01 {
 
     public int somaNaturais(int n){
 
+        //o ponto de para é descrito no enunciado como "se o número for negativo, retorna 0".
         if (n < 0){
             return 0;
         } else{
+            //o número "n" se soma com o "n-1", que seria ele -1 até entrar no ponto de parada"
             return n + somaNaturais(n - 1);
         }
     }
