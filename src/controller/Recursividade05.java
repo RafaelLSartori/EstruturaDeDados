8720beffc3372af354192dae226c311355dbb288
RafaelLSartori <rafa_sartori55@hotmail.com>
2024-08-07 15:58:32 -0300

    Atualização

--- src/controller/Recursividade05.java
+++ src/controller/Recursividade05.java
@@ -6,9 +6,11 @@ public class Recursividade05 {
     }
 
     public double serie(double n){
+        //Ponto de parada pensando que 1/1 = 1
         if(n == 1){
             return 1;
         } else {
+            //chamada da função recursiva variando apenas o denominador, já que o númerador se mantém sem alteração.
             return 1/n + serie(n - 1);
         }
     }
